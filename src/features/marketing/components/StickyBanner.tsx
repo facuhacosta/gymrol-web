9e8755d5d83b08bdd1363571262b080abe7d70a4
facuhacosta <facuacosta200997@hotmail.com>
2026-03-11 15:04:49 -0300

    added adsense

--- src/features/marketing/components/StickyBanner.tsx
+++ src/features/marketing/components/StickyBanner.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { X } from 'lucide-react';
+import { Adsense } from '@ctrl/react-adsense';
 
 const StickyBanner = () => {
   const [isVisible, setIsVisible] = useState(true);
@@ -9,16 +10,12 @@ const StickyBanner = () => {
   return (
     <div className="fixed bottom-0 left-0 right-0 z-40 md:hidden bg-zinc-950 border-t border-electric-blue/30 p-2 shadow-2xl flex justify-center items-center">
       <div className="relative w-[320px] h-[100px] bg-zinc-900 flex items-center justify-center border border-zinc-800 rounded-lg overflow-hidden">
-        <span className="text-[10px] text-zinc-600 font-mono uppercase tracking-widest absolute top-1 right-2">Ad</span>
-        <div className="flex items-center gap-3">
-           <div className="w-12 h-12 bg-electric-green/20 rounded-full flex items-center justify-center">
-              <span className="text-electric-green font-bold text-xl">H</span>
-           </div>
-           <div className="text-left">
-              <p className="text-white font-bold text-sm">HeroHabit PRO</p>
-              <p className="text-zinc-400 text-xs">Sube de nivel 1.5x más rápido</p>
-           </div>
-        </div>
+        <Adsense
+          className='w-[320px] h-[100px] display-block'
+          client="ca-pub-2921918201348815"
+          slot="6232669450"
+          adTest={import.meta.env.VITE_ENV === 'development' ? 'on' : 'off'} //Dev Only
+        />
         
         {/* Close button */}
         <button 
